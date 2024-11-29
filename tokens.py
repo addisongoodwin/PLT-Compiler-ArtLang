45d490a3edd9bdd0dfc1fa2044f5a582de061ed2
addisongoodwin <ag4423@columbia.edu>
2024-11-29 18:33:27 -0500

    added tokens.py, import the token dictionaries in my_scanner, my_parser

---
+++ tokens.py
@@ -0,0 +1,21 @@
+TOKENS = {
+    'TOK_CHARACTERS': 'Characters',
+    'TOK_TRAIT': 'trait',
+    'TOK_EVIL': 'evil',
+    'TOK_STRENGTH': 'strength',
+    'TOK_SCENES': 'Scenes',
+    'TOK_EVENT': 'event',
+    'TOK_LOCATION': 'location',
+    'TOK_YES': 'yes',						# boolean: 1, true
+    'TOK_NO': 'no',							# boolean: 0, false
+    'TOK_NUMBER': 'NUMBER', 				# ints for strength
+    'TOK_EQUALS': '=',
+    'TOK_COMMA': ',',
+    'TOK_IDENTIFIER': 'IDENTIFIER', 		# general variable name
+    'TOK_ERROR': 'error'
+}
+
+INSTRUCTIONS = {
+    'write story': 'TOK_WRITE_STORY_INST',  
+    'print character': 'TOK_PRINT_CHARACTER_INST'
+}
